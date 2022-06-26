1da82a25985a1c9cd12a64bf5332a1f56892d00b
Aphenphos <MaxwillWinters@gmail.com>
2022-06-26 06:43:39 -0400

    added desired search parameters

--- services/bean-service.js
+++ services/bean-service.js
@@ -2,13 +2,21 @@ const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getBeans() {
-    const response = await client
+export async function getBeans(title, color,) {
+    let query = client
         .from('beanie_babies')
         .select(`*`,
             { count: 'exact' });
 
-    return response.data;
+    if (title) {
+        query.ilike(title);
+    }
+
+    if (color) {
+        query.ilike(color);
+    }
+    const response = await query;
+    return response;
 }
 
 export async function getBean(id) {
